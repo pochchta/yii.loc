5e6d93f425ca6210694da57026896015734a9775
pochchta <pochchta@rambler.ru>
2020-07-07 12:47:23 +0800

    Русификация

--- config/web.php
+++ config/web.php
@@ -4,6 +4,8 @@ $params = require __DIR__ . '/params.php';
 $db = require __DIR__ . '/db.php';
 
 $config = [
+    'language' => 'ru-RU',
+    'name' => 'Тест ролей',
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
