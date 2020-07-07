e57b8886687696d94a91e0234ff50baf2e133c45
pochchta <pochchta@rambler.ru>
2020-07-07 12:48:15 +0800

    добавлены виды

---
+++ views/site/sign-up-success.php
@@ -0,0 +1,19 @@
+<?php
+
+/* @var $this yii\web\View */
+/* @var $form yii\bootstrap\ActiveForm */
+/* @var $model app\models\LoginForm */
+
+use yii\helpers\Html;
+
+$this->title = 'Регистрация завершена успешно';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+
+<div class="site-sign-up">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>Вы зарегистрированы под следующим именем:</p>
+    <p><?= Html::encode($model->username) ?></p>
+
+</div>
