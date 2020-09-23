2eadd192b229d353318215e79aaba949324d8bee
pochchta <pochchta@rambler.ru>
2020-09-23 10:18:29 +0800

    исправлена ошибка валидации даты

--- modules/admin/models/AssignmentSearch.php
+++ modules/admin/models/AssignmentSearch.php
@@ -23,7 +23,7 @@ class AssignmentSearch extends AuthAssignment
             [['user_id'], 'integer'],
             [['item_name', 'username'], 'string', 'max' => 64],
             [['item_name', 'username'], 'match', 'pattern' => '/^[\w- ]+$/i'],
-            [['created_at_start', 'created_at_end'], 'integer']
+            [['created_at_start', 'created_at_end'], 'string', 'max' => 64]
         ];
     }
 
