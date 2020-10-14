a5a5a5b3c51a4ee5be0c7a39ad6055f9e8865e71
pochchta <pochchta@rambler.ru>
2020-10-14 10:24:34 +0800

    TODO cache схема

--- config/db.php
+++ config/db.php
@@ -7,7 +7,7 @@ return [
     'password' => '',
     'charset' => 'utf8',
 
-    // Schema cache options (for production environment)
+    // TODO: Schema cache options (for production environment)
     //'enableSchemaCache' => true,
     //'schemaCacheDuration' => 60,
     //'schemaCache' => 'cache',
