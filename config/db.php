fb239db2c028337ab0564e4fa616c5158eaf4161
pochchta <pochchta@rambler.ru>
2020-10-16 09:34:45 +0800

    db localhost -> yii.loc

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=yii.loc;dbname=yii2basic',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
