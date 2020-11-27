0032b339fb310e18091291329781d4d072af7a59
pochchta <pochchta@rambler.ru>
2020-11-27 15:55:16 +0800

    добавлены поля parent_type, parent_id

--- models/Department.php
+++ models/Department.php
@@ -19,16 +19,31 @@ use yii\db\ActiveRecord;
  * @property int|null $created_by
  * @property int|null $updated_by
  * @property int $deleted
+ * @property int $parent_type
+ * @property int $parent_id
  *
- * @property Device[] $devices
+ * @property Device[] $devices magic property
  * @property User|null $creator magic property
  * @property User|null $updater magic property
  */
 class Department extends ActiveRecord
 {
-    const NOT_DELETED = 0;
+    const ALL = -1;                     // для всех свойств
+
+    const NOT_DELETED = 0;              // по умолчанию для свойства deleted
     const DELETED = 1;
-    const ALL = -1;
+
+    const ONLY_DEVICES = 0;             // по умолчанию для свойства parent_type
+    const ONLY_DEPARTMENT = 1;
+//    const DEVICE_AND_DEPARTMENT = 2;
+
+    const LABELS_CONST = [
+        self::ONLY_DEVICES => 'Только приборы',
+        self::ONLY_DEPARTMENT => 'Только цеха',
+//        self::DEVICE_AND_DEPARTMENT => 'Приборы и цеха',
+    ];
+
+    const MAX_LINES_IN_LIST = 100;
     /**
      * {@inheritdoc}
      */
@@ -61,9 +76,11 @@ class Department extends ActiveRecord
     public function rules()
     {
         return [
-            [['name'], 'required'],
+            [['name', 'parent_type', 'parent_id'], 'required'],
             [['description'], 'string'],
             [['name', 'phone'], 'string', 'max' => 255],
+            [['parent_id'], 'exist', 'skipOnError' => true, 'targetAttribute' => ['parent_id' => 'id']],
+            [['parent_type'], 'integer', 'min' => 0, 'max' => 1],
         ];
     }
 
@@ -82,21 +99,46 @@ class Department extends ActiveRecord
             'created_by' => 'Создал',
             'updated_by' => 'Обновил',
             'deleted' => 'Удален',
+            'parent_type' => 'Тип',
+            'parent_id' => 'parent_id',
         ];
     }
 
     /**
      * Gets arr[id] = names of departments
+     * @param int $type
      * @param int $limit
      * @return array
      */
-    public static function getAllNames($limit = 100)
+    public function getAllNames($type = self::ALL, $limit = self::MAX_LINES_IN_LIST)
     {
-        $query = self::find()->select(['id', 'name'])->where(['deleted' => Department::NOT_DELETED])->limit($limit)->asArray()->all();
+        $query = self::find()->select(['id', 'name', 'parent_type', 'parent_id'])->where(['deleted' => Department::NOT_DELETED])->limit($limit);
+        if ($type != self::ALL) {
+            $query->where(['parent_type' => $type]);
+        }
+        $query = $query->asArray()->all();
 
         $outArray = array();
-        foreach ($query as $key => $item) {
-            $outArray[$item['id']] = $item['name'];
+        if ($type == self::ALL) {                   // двухуровневый список
+            foreach ($query as $key => $item) {         // сначала группы
+                if ($item['parent_type'] == self::ONLY_DEPARTMENT) {
+                    $outArray[$item['id']] = $item['name'];
+                    foreach ($query as $keyChild => $itemChild) {
+                        if ($itemChild['parent_id'] == $item['id']) {
+                            $outArray[$item['id']] = '  ' . $item['name'];
+                        }
+                    }
+                }
+            }
+            foreach ($query as $key => $item) {         // затем элементы без группы
+                if ($item['parent_type'] == self::ONLY_DEVICES && $item['parent_id'] == 0) {
+                    $outArray[$item['id']] = $item['name'];
+                }
+            }
+        } else {                                // одноуровневый список
+            foreach ($query as $key => $item) {
+                $outArray[$item['id']] = $item['name'];
+            }
         }
         return $outArray;
     }
