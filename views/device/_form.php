901de13f44cdb0b9ef72b99a78dbe6693ba8c3bf
pochchta <pochchta@rambler.ru>
2021-01-25 15:02:18 +0800

    загрузка через autoComplete

--- views/device/_form.php
+++ views/device/_form.php
@@ -1,31 +1,72 @@
 <?php
 
 use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\jui\AutoComplete;
+use yii\web\JsExpression;
 use yii\widgets\ActiveForm;
-use yii\widgets\Pjax;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Device */
 /* @var $searchModel app\models\DeviceSearch */
 /* @var $form yii\widgets\ActiveForm */
+
+function getAutoCompleteOptions($attribute) {
+    return [
+        'clientOptions' => [
+            'source' => new JsExpression("function(request, response) {
+                $.getJSON('" . Url::to('ajax-one') . "', {
+                term: request.term,
+            }, response);
+        }"),
+            'select' => new JsExpression("function( event, ui ) {
+                $('#device-{$attribute}_id').val(ui.item.id);
+            }"),
+            'minLength' => 3,
+            'delay' => 300
+        ],
+        'options' => [
+            'class' => 'form-control',
+        ]
+    ];
+}
 ?>
 
 <div class="device-form">
 
-    <?php $form = ActiveForm::begin([
-        'id' => 'form1',
-    ]); ?>
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, $attribute ='name')->textInput(['maxlength' => true])->widget(
+        AutoComplete::class, getAutoCompleteOptions($attribute)
+    ); ?>
+    <?= $form->field($model, 'name_id')->hiddenInput()->label(false) ?>
+
+    <?= $form->field($model, $attribute ='type')->textInput(['maxlength' => true])->widget(
+        AutoComplete::class, getAutoCompleteOptions($attribute)
+    ); ?>
+    <?= $form->field($model, 'type_id')->hiddenInput()->label(false) ?>
+
+    <?= $form->field($model, $attribute ='department')->textInput(['maxlength' => true])->widget(
+        AutoComplete::class, getAutoCompleteOptions($attribute)
+    ); ?>
+    <?= $form->field($model, 'department_id')/*->hiddenInput()*/->label(false) ?>
+
+    <?= $form->field($model, $attribute ='position')->textInput(['maxlength' => true])->widget(
+        AutoComplete::class, getAutoCompleteOptions($attribute)
+    ); ?>
+    <?= $form->field($model, 'position_id')/*->hiddenInput()*/->label(false) ?>
+
+    <?= $form->field($model, $attribute ='scale')->textInput(['maxlength' => true])->widget(
+        AutoComplete::class, getAutoCompleteOptions($attribute)
+    ); ?>
+    <?= $form->field($model, 'scale_id')->hiddenInput()->label(false) ?>
+
+    <?= $form->field($model, $attribute ='accuracy')->textInput(['maxlength' => true])->widget(
+        AutoComplete::class, getAutoCompleteOptions($attribute)
+    ); ?>
+    <?= $form->field($model, 'accuracy_id')->hiddenInput()->label(false) ?>
 
     <?= $form->field($model, 'number')->textInput(['maxlength' => true]) ?>
-
-    <?php Pjax::begin([
-        'id' => 'my-pjax-container',
-        'timeout' => Yii::$app->params['pjaxTimeout']
-    ]) ?>
-    <?php echo $this->render('_form_search', [
-        'model' => $searchModel, 'form' => $form
-    ]); ?>
-    <?php Pjax::end() ?>
 
     <?= $form->field($model, 'description')->textarea(['rows' => 6]) ?>
 
