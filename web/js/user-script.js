8007997701e78c37d117d6eaa21a5f6b1732ed3d
pochchta <pochchta@rambler.ru>
2021-02-08 13:56:31 +0800

    обработчики раздельные для type=date и остальных

--- web/js/user-script.js
+++ web/js/user-script.js
@@ -1,36 +1,52 @@
 window.onload = function() {
     if ($("div").is(".incoming-index")) {
+        const TIME_FOR_FILTER = 3000;
         let filter_selector = "#grid_id-filters input, #grid_id-filters select";
-        let timerFilter = new timerForFilter(3000, applyFilter);
+        let filter_text_and_select = "#grid_id-filters :text, #grid_id-filters select";
+        let filter_date = "#grid_id-filters input[type=date]";
+        let timerFilter = new timerForFilter(TIME_FOR_FILTER, applyFilter);
         let keyCode = 0;
+        let enterPressed = false;
         $(document)
             .off("change.yiiGridView keydown.yiiGridView", filter_selector)
             .on('pjax:complete', function() {
                 $(document)
-                    .off("change.yiiGridView keydown.yiiGridView", filter_selector)
+                    .off("change.yiiGridView keydown.yiiGridView", filter_selector);
+                keyCode = 0;
             })
-            .on ("keydown", filter_selector, function(event) {
-                let typeName = $(this).attr("type");
-                keyCode = event.keyCode;
-                if (keyCode === 13) {
-                    if (typeName === "date") {
-                        timerFilter.stop(true);
+            .on ("change keydown", filter_text_and_select, function(event) {
+                if (event.type === "keydown") {
+                    keyCode = event.keyCode;
+                    if (keyCode !== 13) {
+                        return;
+                    } else {
+                        enterPressed = true;
                     }
-                    keyCode = 0;
-                    $(this).blur();
+                } else {
+                    if (enterPressed) {
+                        enterPressed = false;
+                        return;
+                    }
                 }
+                applyFilter();
             })
-            .on ("change", filter_selector, function() {
-                let typeName = $(this).attr("type");
-                if (typeName === "date") {
-                    if (keyCode === 0) {
+            .on ("change keydown", filter_date, function(event) {
+                if (event.type === "keydown") {
+                    keyCode = event.keyCode;
+                    if (keyCode === 13) {
+                        if (timerFilter.stop(true) === false) {
+                            applyFilter();
+                        }
+                    }
+                } else {
+                    if (keyCode === 0 || keyCode === 13) {
                         applyFilter();
                     } else {
-                        timerFilter.stop().start();
+                        timerFilter.stop();
+                        if (this.value !== "") {
+                            timerFilter.start();
+                        }
                     }
-                    keyCode = 0;
-                } else {
-                    applyFilter();
                 }
             })
     }
@@ -49,13 +65,18 @@ class timerForFilter
         this.func = func;
     }
 
+    // return bool был ли таймер запущен
     start()
     {
-        this.flagComplete = false;
-        this.timerId = setTimeout(this.process.bind(this), this.time);
-        return this;
+        if (this.flagComplete) {
+            this.flagComplete = false;
+            this.timerId = setTimeout(this.process.bind(this), this.time);
+            return true;
+        }
+        return false;
     }
 
+    // return bool был ли таймер остановлен
     stop(forceCall = false)
     {
         if (this.flagComplete === false) {
@@ -64,8 +85,9 @@ class timerForFilter
                 this.func();
             }
             this.flagComplete = true;
+            return true;
         }
-        return this;
+        return false;
     }
 
     process()
