5b7979780c8f2123266d0dc11caef26024595eb2
pochchta <pochchta@rambler.ru>
2021-09-27 13:25:34 +0800

    подготовка данных для построения меню вынесена в класс FilterMenu

---
+++ models/FilterMenu.php
@@ -0,0 +1,51 @@
+<?php
+
+
+namespace app\models;
+
+
+class FilterMenu
+{
+    private $headerMenu, $listSource = [], $listLabel = [];
+    const DEFAULT_SOURCE = 'word';
+
+    public function __construct($headerMenu)
+    {
+        $this->headerMenu = $headerMenu;
+    }
+
+    public function setSource($listSource)
+    {
+        $this->listSource = $listSource;
+        return $this;
+    }
+
+    public function setLabel($listLabel)
+    {
+        $this->listLabel = $listLabel;
+        return $this;
+    }
+
+    public function getMenu()
+    {
+        $menu = [];
+        foreach ($this->headerMenu as $item) {
+            $source = self::DEFAULT_SOURCE;
+            if (isset($this->listSource[$item])) {
+                $source = $this->listSource[$item];
+            }
+            if ($source === self::DEFAULT_SOURCE) {
+                $menu[$item] = WordSearch::findNamesByParentId(['parent_id' => $id = Word::getFieldWord($item)]);
+                $menu[$item]['name'] = Word::LABEL_FIELD_WORD[$id];
+                $menu[$item]['id'] = $id;
+            } else {
+                $menu[$item]['id'] = $item;
+            }
+            $menu[$item]['source'] = $source;
+            if (isset($this->listLabel[$item])) {
+                $menu[$item]['name'] = $this->listLabel[$item];
+            }
+        }
+        return $menu;
+    }
+}
