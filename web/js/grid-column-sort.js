3a2dbeb9dcc4a0431d2e779792e6c48bf7b2d63c
pochchta <pochchta@rambler.ru>
2022-08-23 14:11:50 +0800

    виджет - js - отправка на сервер

--- web/js/grid-column-sort.js
+++ web/js/grid-column-sort.js
@@ -1,3 +1,33 @@
+function takeColumnsFromHtml() {
+    let $elements = $('#grid_column_sort #sortable1 li');
+    return Array.from($elements).map(item => item.innerText);
+}
+
+/**
+ * Сохранение выбранных и отсортированных столбцов
+ */
+function saveGridColumnSort(e) {
+    $.ajax({
+        method: "POST",
+        url: e.data.writeUrl,
+        data: {
+            name: e.data.name,
+            role: e.data.role,
+            col: JSON.stringify(takeColumnsFromHtml()),
+        },
+        success: function (msg) {
+            console.log('success');
+            console.log(msg);
+
+        },
+        complete: function (jqXHR, textStatus) {
+            console.log(textStatus);
+            if (textStatus !== 'success') {
+                console.log('ошибка')
+            }
+        }
+    });
+}
 
 $( document ).ready(function(){
     $(function() {
