fedffc3ea9f321f5c566a99a60ec4134a68e7880
pochchta <pochchta@rambler.ru>
2022-11-18 10:35:46 +0800

    реорганизация

--- widgets/grid-column-sort/src/ViewRender.php
+++ widgets/grid-column-sort/src/ViewRender.php
@@ -13,6 +13,13 @@ class ViewRender extends Widget
         $view = $this->getView();
         $options = json_encode($this->clientOptions['params']);
         $view->registerJs("$('#save_grid_column_sort').on('click', $options, saveGridColumnSort);");
-        return $this->render('widget');
+        return $this->render('widget', ['columns' => $this->formatColumns()]);
     }
+
+    protected function formatColumns()
+    {
+        $columns = $this->clientOptions['columns'];
+        return $columns;
+    }
+
 }
--- widgets/grid-column-sort/src/views/widget.php
+++ widgets/grid-column-sort/src/views/widget.php
@@ -1,21 +1,9 @@
 <div id='grid_column_sort' class='clearfix'>
-
     <ul id='sortable1' class='connectedSortable'>
-        <li class='ui-state-default'>Item 1</li>
-        <li class='ui-state-default'>Item 2</li>
-        <li class='ui-state-default'>Item 3</li>
-        <li class='ui-state-default'>Item 4</li>
-        <li class='ui-state-default'>Item 5</li>
+        <? foreach($columns['enabled'] as $key => $value) echo "<li id='$key' class='ui-state-default'>$value</li>" ?>
     </ul>
-
     <ul id='sortable2' class='connectedSortable'>
-        <li class='ui-state-highlight'>Item 1</li>
-        <li class='ui-state-highlight'>Item 2</li>
-        <li class='ui-state-highlight'>Item 3</li>
-        <li class='ui-state-highlight'>Item 4</li>
-        <li class='ui-state-highlight'>Item 5</li>
+        <? foreach($columns['disabled'] as $key => $value) echo "<li id='$key' class='ui-state-highlight'>$value</li>" ?>
     </ul>
-
     <button class='hide_grid_column_sort' id='save_grid_column_sort'>Сохранить</button>
-
 </div>
