330801fc1718520c68e1306ed5c5af033eed3599
pochchta <pochchta@rambler.ru>
2022-11-22 10:45:56 +0800

    действия в профиле разбиты на разные формы и виды

---
+++ models/profile/ChangePassForm.php
@@ -0,0 +1,103 @@
+<?php
+
+
+namespace app\models\profile;
+
+use app\models\User;
+use Yii;
+use yii\base\Model;
+use yii\base\Exception;
+use yii\web\NotFoundHttpException;
+
+class ChangePassForm  extends Model
+{
+    public $oldPass;                   // старый пароль
+    public $newPass;                   // новый пароль
+    public $newPassRepeat;             // подтверждение нового пароля
+
+    private $_userById;
+
+    public function rules()
+    {
+        return [
+            [['oldPass', 'newPass', 'newPassRepeat'], 'required'],
+            [['oldPass', 'newPass', 'newPassRepeat'], 'string', 'min' => 1, 'max' => 64],
+            [['newPass'], 'validateChangedPass'],
+            [['newPassRepeat'], 'validatePassRepeat'],
+            [['oldPass'], 'validateOldPass'],
+        ];
+    }
+
+    public function validateOldPass($attribute)
+    {
+        if (!$this->hasErrors()) {
+            $user = $this->_userById;
+            if ($user->validatePassword($this->oldPass) == false) {
+                $this->addError($attribute, 'Старый пароль не верен');
+            }
+        }
+    }
+
+    public function validateChangedPass($attribute)
+    {
+        if (!$this->hasErrors()) {
+            if ($this->oldPass === $this->newPass) {
+                $this->addError($attribute, 'Старый и новый пароль одинаковы');
+            }
+        }
+    }
+
+    public function validatePassRepeat($attribute)
+    {
+        if (!$this->hasErrors()) {
+            if ($this->newPass !== $this->newPassRepeat) {
+                $this->addError($attribute, 'Новый пароль и повторный новый пароль не совпадают');
+            }
+        }
+    }
+
+    public function __construct($config = [])
+    {
+        parent::__construct($config);
+        $this->_userById = User::findOne(Yii::$app->user->identity->id);
+    }
+
+    /**
+     * Обновление данных пользователя
+     * @throws Exception
+     */
+    public function updatePass()
+    {
+        if ($this->validate()) {
+            $user = $this->_userById;
+            if ($user === NULL) {
+                throw new NotFoundHttpException('Запрошенная страница не существует.');
+            }
+
+            $user->password = Yii::$app->getSecurity()->generatePasswordHash($this->newPass);
+            if ($user->save()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Очистка полей
+     */
+    public function clearFields()
+    {
+        $this->oldPass = '';
+        $this->newPass = '';
+        $this->newPassRepeat = '';
+    }
+
+    public function attributeLabels()
+    {
+        return [
+            'oldPass' => 'Старый пароль',
+            'newPass' => 'Новый пароль',
+            'newPassRepeat' => 'Повтор нового пароля',
+        ];
+    }
+}
---
+++ views/site/profile/change-pass.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+$this->title = 'Профиль';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+
+<h1>Профиль</h1>
+
+<div class="row">
+
+    <div class="col-xs-4">
+        <?= $this->render('menu') ?>
+    </div>
+
+    <div class="col-xs-8">
+        <h4>Смена пароля</h4>
+
+        <div class="user-form">
+
+            <?php $form = ActiveForm::begin(); ?>
+
+            <?= $form->field($model, 'oldPass')->passwordInput(['maxlength' => true])
+            ?>
+
+            <?= $form->field($model, 'newPass')->passwordInput(['maxlength' => true])
+            ?>
+
+            <?= $form->field($model, 'newPassRepeat')->passwordInput(['maxlength' => true])
+            ?>
+
+            <div class="form-group">
+                <?= Html::submitButton('Сохранить', ['class' => 'btn btn-success']) ?>
+            </div>
+
+            <?php ActiveForm::end(); ?>
+
+        </div>
+    </div>
+</div>
