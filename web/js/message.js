4e8abaee605ca8f32e19baf89284bb4b66555564
pochchta <pochchta@rambler.ru>
2022-12-05 15:04:10 +0800

    изменен порядок добавления сообщений: новое снизу

--- web/js/message.js
+++ web/js/message.js
@@ -7,10 +7,21 @@ function showMessage(text = 'Success', type = 'success') {
     if (type !== 'success') {
         type = 'danger';
     }
-    $(
+    let $container = $('body>.wrap>.container');
+    let $message = $(
         '<div class="alert-' + type + ' alert fade in">' +
         '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>' +
         text +
         '</div>'
-    ).insertAfter('body>.wrap>.container>.breadcrumb')
+    );
+    let $lastAlert = $container.children('.alert').last();
+    let $breadcrumb = $container.children('.breadcrumb').last();
+
+    if ($lastAlert.length) {
+        $message.insertAfter($lastAlert);
+    } else if ($breadcrumb.length) {
+        $message.insertAfter($breadcrumb);
+    } else {
+        $container.prepend($message);
+    }
 }
