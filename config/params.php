18ed16725a325693ab57a4ede5c22f7d01a5434c
pochchta <pochchta@rambler.ru>
2023-02-16 12:51:25 +0800

    комментарии

--- config/params.php
+++ config/params.php
@@ -5,19 +5,25 @@ return [
     'senderEmail' => 'noreply@example.com',
     'senderName' => 'Example.com mailer',
 
+    // http cache
+    'cacheControlTime' => 24*60*60,     // seconds
+
+    // mutex
     'mutexTimeout' => 1,                // seconds
 
-    'pjaxTimeout' => 5000,              // milliSeconds
-
-    'delayDateInput' => 1000,           // ms, задержка перед отправкой фильтра input type = date
-
+    // html
     'maxLengthTextField' => 40,         // максимальная длина текстовых полей
     'maxLengthSearchParam' => 40,       // максимальная длина параметра поиска
+
+    // javascript
+    'pjaxTimeout' => 5000,              // milliSeconds
 
     'delayAutoComplete' => 500,
     'minSymbolsAutoComplete' => 3,
     'maxLinesAutoComplete' => 10,       // кол-во строк в autoComplete
     'maxElementsTabMenu' => 1000,       // кол-во строк во вкладке меню фильтров
+
+    'delayDateInput' => 1000,           // ms, задержка перед отправкой фильтра input type = date
 
     'maxLinesIndex' => 20,              // кол-во строк при выводе в index
     'maxLinesPrint' => 500,             // кол-во строк при выводе на печать
