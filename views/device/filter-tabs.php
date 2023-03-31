23d0e336a9efe9e46110a055e55f67ce9f8bfcee
pochchta <pochchta@rambler.ru>
2023-03-31 10:16:41 +0800

    position: absolute добавлен класс .absolute

--- views/device/filter-tabs.php
+++ views/device/filter-tabs.php
@@ -6,16 +6,16 @@ use yii\helpers\Html;
 /* @var $menu app\models\FilterMenu */
 ?>
 
-<form id="filters-form" action="" method="get">
+<form id="filters-form">
     <div class="catalogTabs">
         <div class="tabs_title" id="tabs">
             <ul>
                 <?php foreach ($menu->getMenu() as $key => $tab): ?>
-                    <li><a data-value="tab<?=$tab['id']?>" data-name="<?=$tab['name']?>"><span><?=$tab['label']?></span></a></li>
+                    <li><a data-value="<?=$tab['id']?>" data-name="<?=$tab['name']?>"><span><?=$tab['label']?></span></a></li>
                 <?php endforeach ?>
             </ul>
         </div>
-        <div class="tabs_content hide" id="tabs_content1">
+        <div class="tabs_content hide absolute" id="tabs_content1">
 
             <?php foreach ($menu->getMenu() as $key => $tab): ?>
                 <div id="tab<?=$tab['id']?>" data-name="<?=$key?>" class="hide">
@@ -23,31 +23,29 @@ use yii\helpers\Html;
                         <?= Html::input('text', $key) ?>
                         <?= Html::button('Применить', ['class' => 'filter_button']) ?>
                     <?php elseif($tab['source'] === 'manual'): ?>
+                        <?= Html::input('text', $key, '', ['class' => 'hide']) ?>
                         <div class="checkboxList">
-                            <?= Html::input('text', $key, '', ['class' => 'hide']) ?>
-                            <span class="checkbox filter-checkbox" data-child="0" data-value="">Действующие</span>
-                            <span class="checkbox filter-checkbox" data-child="0" data-value="<?=Status::DELETED?>">Удаленные</span>
-                            <span class="checkbox filter-checkbox" data-child="0" data-value="<?=Status::ALL?>">Все</span>
+                            <span class="checkbox filter-checkbox" data-source=<?=$key?> data-value="<?=Status::NOT_DELETED?>">Действующие</span>
+                            <span class="checkbox filter-checkbox" data-source=<?=$key?> data-value="<?=Status::DELETED?>">Удаленные</span>
+                            <span class="checkbox filter-checkbox" data-source=<?=$key?> data-value="<?=Status::ALL?>">Все</span>
                         </div>
                     <?php elseif($tab['source'] === 'date'): ?>
-                            <?= Html::input('date', $key . '_start') ?>
-                            <?= Html::input('date', $key . '_end') ?>
+                        <?= Html::input('date', $key . '_start') ?>
+                        <?= Html::input('date', $key . '_end') ?>
                         <?= Html::button('Применить', ['class' => 'filter_button']) ?>
                     <?php else: ?>
-                        <div class="checkboxList">
-                            <?= Html::input('text', $key, '', ['class' => 'hide']) ?>
-                            <?php foreach ($tab as $elem): if (is_array($elem) == false) continue;?>
-                                <span class="checkbox filter-checkbox" data-value=<?=$elem['id']?>><?=$elem['name']?></span>
-                            <?php endforeach ?>
-                        </div>
+                        <?= Html::input('text', $key, '') ?>
+                        <?= Html::input('text', $key . '_id', '', ['class' => 'hide']) ?>
+                        <?= Html::button('Применить', ['class' => 'filter_button']) ?>
+                        <div class="checkboxList"></div>
                     <?php endif ?>
                 </div>
             <?php endforeach ?>
 
             <div id="block_arrow1" class="block_arrow glyphicon glyphicon-menu-down hide"></div>
-            <div class="tabs_content hide" id="tabs_content2">
+            <div class="tabs_content hide absolute" id="tabs_content2">
                 <div id="block_arrow2" class="block_arrow glyphicon glyphicon-menu-down hide"></div>
-                <div class="tabs_content hide" id="tabs_content3"></div>
+                <div class="tabs_content hide absolute" id="tabs_content3"></div>
             </div>
         </div>
         <div class="tabsFilterParams hide">
