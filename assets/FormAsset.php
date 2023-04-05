e31eb44891c99f03ed2bc8cbf016b08d68a634c1
pochchta <pochchta@rambler.ru>
2023-04-05 13:01:16 +0800

    разные ресурсы под разные виды меню

--- assets/FormAsset.php
+++ assets/FormAsset.php
@@ -3,17 +3,17 @@ namespace app\assets;
 
 use yii\web\AssetBundle;
 
-class GridAsset extends AssetBundle
+class FormAsset extends AssetBundle
 {
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-
+        'css/catalog-tabs.css',
     ];
     public $js = [
         'js/init-grid.js',
         'js/catalog-tabs/main.js',
-        'js/catalog-tabs/grid.js',
+        'js/catalog-tabs/form.js',
     ];
     public $depends = [
         'app\assets\AppAsset',
