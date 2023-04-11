12dea145ff110993ae6926080dd6add838a6d34d
pochchta <pochchta@rambler.ru>
2023-04-11 12:52:08 +0800

    поисковая модель для api

---
+++ modules/api/models/WordSearch.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace app\modules\api\models;
+
+use app\models\Status;
+use app\models\Word;
+use Yii;
+use yii\base\Model;
+
+/**
+ * WordSearch represents the model behind the search form of `app\models\Word`.
+ */
+class WordSearch extends Word
+{
+    const REPLACE_NAMES = ['value'];
+
+    public $limit;
+    public $parent, $parent_v;
+    public $name_v;
+    public $replace_name;
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'created_at', 'updated_at', 'created_by', 'updated_by', 'deleted', 'parent_id'], 'integer'],
+            [['name', 'name_v', 'value', 'description'], 'string', 'max' => Yii::$app->params['maxLengthSearchParam']],
+            [['deleted'], 'default', 'value' => Status::NOT_DELETED],
+            [['parent', 'parent_v'], 'string', 'max' => Yii::$app->params['maxLengthSearchParam']],
+            [['limit'], 'integer', 'min' => 0, 'max' => Yii::$app->params['maxLines']],
+            [['replace_name'], 'validateReplaceName'],
+        ];
+    }
+
+    public function validateReplaceName($attribute)
+    {
+        if (!$this->hasErrors()) {
+            if (in_array($this->replace_name, $this::REPLACE_NAMES) == false) {
+                $this->addError($attribute, 'Недопустимое имя для замены');
+                    return;
+            }
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    public function __construct($config = [])
+    {
+        parent::__construct($config);
+        $this->limit = Yii::$app->params['maxLinesIndex'];
+    }
+
+    /**
+     * @return array
+     */
+    public function findNames()
+    {
+        $names = [];
+        if ($this->validate()) {
+            $query = Word::find();
+
+            if (mb_strlen($this->replace_name)) {
+                $query->select(['name as ' . $this->replace_name]);
+            } else {
+                $this->replace_name = 'name';
+                $query->select(['name']);
+            }
+
+            if (mb_strlen($this->name)) {
+                $query->andFilterWhere(['like', 'name', $this->name . '%', false]);
+            }
+            if (mb_strlen($this->name_v)) {
+                $query->andFilterWhere(['like', 'name', $this->name_v . '%', false]);
+                $names = array_map(function ($item) {
+                    return [$this->replace_name => Word::LABEL_FIELD_WORD[$item]];
+                }, Word::getNumbersBySimilarLabel($this->name_v . '%'));
+            }
+
+            $names = array_slice($names, 0, $this->limit);  // обрезка если уже слишком много элементов
+            $limit = $this->limit - count($names);
+            if ($limit > 0) {
+                $names = array_merge($names,
+                    $query
+//                        ->where(['deleted' => Status::NOT_DELETED])
+//                        ->distinct()
+                        ->orderBy('name')
+                        ->limit($limit)
+                        ->asArray()
+                        ->all()
+                );
+            }
+
+        }
+        return $names;
+    }
+}
