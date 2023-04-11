693dcecdbf6c24faead2a6fb8ef96485b436979f
pochchta <pochchta@rambler.ru>
2023-04-11 13:23:07 +0800

    упорядочил findNames

--- modules/api/models/WordSearch.php
+++ modules/api/models/WordSearch.php
@@ -60,47 +60,84 @@ class WordSearch extends Word
         $this->limit = Yii::$app->params['maxLinesIndex'];
     }
 
-    /**
+    /** Поиск имен в базе и массиве Word::LABEL_FIELD_WORD по name_id, name (по базе), name_v (включая массив).
+     * replace_name - замена ключа,
+     * limit - лимит
      * @return array
      */
     public function findNames()
     {
         $names = [];
         if ($this->validate()) {
-            $query = Word::find();
-
-            if (mb_strlen($this->replace_name)) {
-                $query->select(['name as ' . $this->replace_name]);
-            } else {
-                $this->replace_name = 'name';
-                $query->select(['name']);
-            }
-
-            if (mb_strlen($this->name)) {
-                $query->andFilterWhere(['like', 'name', $this->name . '%', false]);
-            }
             if (mb_strlen($this->name_v)) {
-                $query->andFilterWhere(['like', 'name', $this->name_v . '%', false]);
                 $names = array_map(function ($item) {
-                    return [$this->replace_name => Word::LABEL_FIELD_WORD[$item]];
+                    $name = $this->replace_name ?? 'name';
+                    return [$name => Word::LABEL_FIELD_WORD[$item]];
                 }, Word::getNumbersBySimilarLabel($this->name_v . '%'));
             }
-
             $names = array_slice($names, 0, $this->limit);  // обрезка если уже слишком много элементов
+
             $limit = $this->limit - count($names);
             if ($limit > 0) {
-                $names = array_merge($names,
-                    $query
-//                        ->where(['deleted' => Status::NOT_DELETED])
-//                        ->distinct()
-                        ->orderBy('name')
-                        ->limit($limit)
-                        ->asArray()
-                        ->all()
-                );
+                $query = Word::find()
+//                    ->where(['deleted' => Status::NOT_DELETED])
+//                    ->distinct()
+                    ->andFilterWhere(['id' => $this->id])
+                    ->orderBy('name')
+                    ->limit($limit)
+                    ->asArray();
+
+                if (mb_strlen($this->replace_name)) {
+                    $query->select(['name as ' . $this->replace_name]);
+                } else {
+                    $query->select(['name']);
+                }
+
+                if (mb_strlen($this->name)) {
+                    $query->andFilterWhere(['like', 'name', $this->name . '%', false]);
+                }
+                if (mb_strlen($this->name_v)) {
+                    $query->andFilterWhere(['like', 'name', $this->name_v . '%', false]);
+                }
+
+                $names = array_merge($names, $query->all());
             }
 
         }
+        return $names;
+    }
+
+    /** Поиск элементов по parent_id, parent, parent_v
+     * @return array [['id' => 1, 'value' => 'Название'], []]
+     */
+    public function findNamesByParent()
+    {
+        $names = [];
+        if ($this->validate()) {
+            $query = Word::find()
+                ->select(['id', 'name'])
+                ->orderBy('name')
+//            ->distinct()
+                ->asArray()
+                ->limit($this->limit)
+                ->where(['deleted' => Status::NOT_DELETED])
+                ->andFilterWhere([
+                    'parent_id' => $this->parent_id,
+                ]);
+            if (strlen($this->parent)) {
+                $query->andFilterWhere([
+                    'id' => Word::getQueriesToGetChildren(['like', 'name', $this->parent . '%', false])[1]
+                ]);
+            }
+            if (strlen($this->parent_v)) {
+                $subQuery = ['or'];
+                $subQuery[]['id'] = Word::getQueriesToGetChildren(['like', 'name', $this->parent_v . '%', false])[1];
+                $subQuery[]['id'] = Word::getQueriesToGetChildrenIfParentIsVirtual(['like', 'name', $this->parent_v . '%', false])[1];
+                $query->andFilterWhere($subQuery);
+            }
+            $names = $query->all();
+        }
+
         return $names;
     }
 }
