2e64941962f3b7369f2d7002e91c48150c021edd
pochchta <pochchta@rambler.ru>
2023-04-21 11:23:56 +0800

    меню

--- views/device/update.php
+++ views/device/update.php
@@ -15,7 +15,7 @@ $this->params['breadcrumbs'][] = 'Изменение';
     <h1><?= Html::encode($this->title) ?></h1>
 
     <?= $this->render('_form', compact(
-        'model'
+        'model', 'menu'
     )); ?>
 
 </div>
