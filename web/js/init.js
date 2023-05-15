7b4aec487e9ad3c8a9c936437b9a51574266dc5c
pochchta <pochchta@rambler.ru>
2023-05-15 11:26:47 +0800

    gettingVersion для разных источников

--- web/js/init.js
+++ web/js/init.js
@@ -18,49 +18,68 @@ function initHideShowByToggleId() {
         .on('click', function(event) {              // hide/show by toggleId
             let id = event.target.dataset.toggleId;
             if (!id) return;
-            let elem = document.getElementById(id);
-            elem.hidden = !elem.hidden;
+            $('#' + id).toggle();
         })
 }
 
 $(window).on('load', function() {
+    window.gettingVersion = {};
+    window.gettingVersion.word = gettingData('/api/word/get-version');
+    window.gettingVersion.device = gettingData('/api/device/get-version');
+    window.gettingVersion.verification = gettingData('/api/verification/get-version');
 
     window.gettingToken = gettingData('/api/user/get-token');
-    window.gettingWordVersion = gettingData('/api/word/get-version');
     window.gettingYiiParams = gettingData('/api/app/get-params');
+    window.gettingRulesAutoComplete = gettingData('/api/auto-complete/get-rules');
     window.gettingYiiParams().done(function (data) {
         window.yiiParams = data;
     });
 
     initHideShowByToggleId();
     addAutoCompleteOptions();
-
 })
 
 function addAutoCompleteOptions() {
     window.gettingYiiParams().done(function (params) {
-        window.gettingWordVersion().done(function (version) {
+        window.gettingRulesAutoComplete().done(function (rules) {
             let $inputs = $('#active-form input.ui-autocomplete-input, #filters-form input.ui-autocomplete-input');
 
             for (let input of $inputs) {
                 let $input = $(input);
-                $input.autocomplete({
-                    "source": function (request, response) {
-                        $.getJSON('/api/word/get-auto-complete', {
-                            name: request.term,
-                            field: $input.attr('name'),
-                            parent: $input.attr('data-parent'),
-                            version: version,
-                        }, response);
-                    },
-                    "minLength": params['minSymbolsAutoComplete'],
-                    "delay": params['delayAutoComplete'],
-                    "select": function (event, ui) {
-                        onSelectAutoComplete(event, ui)
-                    }
-                });
-            }
+                let source = 'word';
+                let fieldName = $input.attr('name');
+                let parentName = $input.attr('data-parent');
 
+                if (
+                    rules.hasOwnProperty(parentName)
+                    && rules[parentName].hasOwnProperty(fieldName)
+                    && rules[parentName][fieldName].hasOwnProperty('source')
+                    && rules[parentName][fieldName]['source'] === 1
+                ) {
+                    source = parentName.split('_')[0];
+                }
+
+                window.gettingVersion[source]().done(function (version) {
+
+                    $input.autocomplete({
+                        source: function (request, response) {
+                            $.getJSON('/api/word/get-auto-complete', {
+                                name: request.term,
+                                field: $input.attr('name'),
+                                parent: $input.attr('data-parent'),
+                                version: version,
+                            }, response);
+                        },
+                        minLength: params['minSymbolsAutoComplete'],
+                        delay: params['delayAutoComplete'],
+                        select: function (event, ui) {
+                            onSelectAutoComplete(event, ui)
+                        },
+                        appendTo: ".catalogTabs"
+                    });
+
+                })
+            }
         })
     })
 }
