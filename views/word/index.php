11897aa6db286be5ff0968ac2126006172dc5d2d
pochchta <pochchta@rambler.ru>
2023-05-23 11:41:41 +0800

    добавлен catalogTabsSort

--- views/word/index.php
+++ views/word/index.php
@@ -1,27 +1,41 @@
 <?php
 
+use app\assets\GridAsset;
 use app\models\Word;
 use app\models\Status;
-use app\models\WordSearch;
+use app\widgets\csc\CatalogTabsSort;
 use yii\helpers\Html;
 use yii\grid\GridView;
-use yii\jui\AutoComplete;
 use yii\widgets\Pjax;
 
 /* @var $this yii\web\View */
-/* @var $searchModel app\models\WordSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
 $this->title = 'Словарь';
 $this->params['breadcrumbs'][] = $this->title;
+
+GridAsset::register($this);
+
+$catalogTabsSort = new CatalogTabsSort($menu, [
+    'required' => Yii::$app->user->can('ChangingCatalogTabsSort') ? ['Настройки'] : [],
+    'token' => Yii::$app->user->identity->getAuthKey(),
+    'write_url' => '/api/csc/write-column',
+    'read_url' => '/api/csc/read-column',
+    'class' => '\app\models\word',
+    'role' => Yii::$app->user->identity->getProfileView(),
+]);
 ?>
-<div class="word-index">
+<div class="word-index page-index" id="page-index">
 
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
         <?= Html::a('Создать запись', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
+
+    <?= $this->render('/catalog-tabs/grid', compact(
+        'catalogTabsSort'
+    )); ?>
 
     <?php Pjax::begin([
         'id' => 'my-pjax-container',
@@ -31,7 +45,6 @@ $this->params['breadcrumbs'][] = $this->title;
     <?= GridView::widget([
         'id' => 'grid_id',
         'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
         'columns' => [
             ['class' => 'yii\grid\SerialColumn'],
 
@@ -41,17 +54,9 @@ $this->params['breadcrumbs'][] = $this->title;
                 'value' => function ($model) {
                     return Html::a($model->name, ['view', 'id' => $model->id]);
                 },
-                'filter' => AutoComplete::widget([
-                    'model' => $searchModel,
-                    'attribute' => $attribute,
-                ] + WordSearch::getAutoCompleteOptions($attribute))
             ],
             [
                 'attribute' => $attribute = 'value',
-                'filter' => AutoComplete::widget([
-                    'model' => $searchModel,
-                    'attribute' => $attribute,
-                ] + WordSearch::getAutoCompleteOptions($attribute))
             ],
 //            'description:ntext',
             [
@@ -61,11 +66,6 @@ $this->params['breadcrumbs'][] = $this->title;
                     $parent = Word::getParentByLevel($model, 0);
                     return $parent->name;
                 },
-                'filter' => Html::activeDropDownList(
-                    $searchModel,
-                    $attribute = 'category1',
-                    [Status::ALL => 'все'] + array_combine(array_keys(Word::FIELD_WORD), Word::LABEL_FIELD_WORD)
-                )
             ],
             [
                 'attribute' => $attribute = 'category2',
@@ -76,10 +76,6 @@ $this->params['breadcrumbs'][] = $this->title;
                     }
                     return NULL;
                 },
-                'filter' => AutoComplete::widget([
-                    'model' => $searchModel,
-                    'attribute' => $attribute,
-                ] + WordSearch::getAutoCompleteOptions($attribute))
             ],
             [
                 'attribute' => $attribute = 'category3',
@@ -90,25 +86,7 @@ $this->params['breadcrumbs'][] = $this->title;
                     }
                     return NULL;
                 },
-                'filter' => AutoComplete::widget([
-                    'model' => $searchModel,
-                    'attribute' => $attribute,
-                ] + WordSearch::getAutoCompleteOptions($attribute))
             ],
-/*            [
-                'attribute' => $attribute = 'category4',
-                'format' => 'html',
-                'value' => function ($model) {
-                    if ($parent = Word::getParentByLevel($model, 3)) {
-                        return Html::a($parent->name, ['view', 'id' => $parent->id]);
-                    }
-                    return NULL;
-                },
-                'filter' => AutoComplete::widget([
-                    'model' => $searchModel,
-                    'attribute' => $attribute,
-                ] + WordSearch::getAutoCompleteOptions($attribute))
-            ],*/
             [
                 'attribute' => 'deleted_id',
                 'format' => 'html',
@@ -119,13 +97,20 @@ $this->params['breadcrumbs'][] = $this->title;
                         return '<span class="glyphicon glyphicon-remove-sign color-err" title="Удален"></span>';
                     }
                 },
-                'filter' => Html::activeDropDownList($searchModel, 'deleted', [
-                    Status::NOT_DELETED => 'нет',
-                    Status::DELETED => 'да',
-                    Status::ALL => 'все'
-                ])
             ],
             [
+                'header' =>
+                    Html::a(
+                        '<span class="glyphicon glyphicon-remove reset_sort a-action"></span>',
+                        null,
+                        ['title' => 'Сбросить сортировку']
+                    )
+/*                    . Html::a(
+                        '<span class="glyphicon glyphicon-cog show_grid_column_sort a-action" data-toggle-id="grid_column_sort"></span>',
+                        null,
+                        ['title' => 'Настроить столбцы']
+                    )*/,
+                'contentOptions' => ['class' => 'nowrap'],
                 'format' => 'raw',
                 'filter' => Html::a(
                     '<span class="glyphicon glyphicon-remove a-action"></span>',
